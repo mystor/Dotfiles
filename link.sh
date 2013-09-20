c72dd9d9da06057ca106d178932cab2d57f562fd
Michael Layzell <michael@redrasor.com>
2013-09-20 14:29:57 -0400

    Fix powerline

--- link.sh
+++ link.sh
@@ -1,11 +1,11 @@
 # Link .vim
-ln -s vim ~/.vim
-ln -s vim/vimrc ~/.vimrc
+ln -s ~/Dotfiles/vim ~/.vim
+ln -s ~/Dotfiles/vim/vimrc ~/.vimrc
 
 # Link shell rcs
-ln -s sh/bashrc ~/.bashrc
-ln -s sh/bash_profile ~/.bash_profile
-ln -s sh/zshrc ~/.zshrc
+ln -s ~/Dotfiles/sh/bashrc ~/.bashrc
+ln -s ~/Dotfiles/sh/bash_profile ~/.bash_profile
+ln -s ~/Dotfiles/sh/zshrc ~/.zshrc
 
 # Link emacs
-ln -s emacs ~/.emacs.d
+ln -s ~/Dotfiles/emacs ~/.emacs.d
--- sh/common.sh
+++ sh/common.sh
@@ -5,3 +5,8 @@
 
 # Vim like keybindings in the shell
 set -o vi
+
+# Add powerline to the PATH
+
+export PATH=$PATH:~/Dotfiles/powerline/scripts
+export PYTHONPATH=$PYTHONPATH:~/Dotfiles/powerline
