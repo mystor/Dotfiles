145d123d842eab860efb377ba9e4e238c0be3e7c
vagrant <vagrant@precise32.(none)>
2013-09-27 18:17:40 +0000

    Added vim aliases with screen-256color

--- sh/common.sh
+++ sh/common.sh
@@ -7,6 +7,9 @@
 set -o vi
 
 # Add powerline to the PATH
-
 export PATH=$PATH:~/Dotfiles/powerline/scripts
 export PYTHONPATH=$PYTHONPATH:~/Dotfiles/powerline
+
+# Make vim work better
+alias vim='TERM=screen-256color vim'
+alias v='TERM=screen-256color vim'
