e9914bcac775dab484107295701c4118d50ca31b
Michael Layzell <michael@redrasor.com>
2013-11-11 23:15:49 -0500

    Added virtualenvwrapper

--- sh/common.sh
+++ sh/common.sh
@@ -16,3 +16,11 @@ alias v='TERM=screen-256color vim'
 
 # Make emacs nicer
 alias ec='emacsclient -c --alternate-editor ""'
+
+# Enable virtualenvwrapper
+VIRTUAL_ENV="/usr/local/bin/virtualenvwrapper.sh"
+if [ -f "$VIRTUAL_ENV" ]; then
+	export WORKON_HOME="~/.virtualenvs"
+	mkdir -p "$WORKON_HOME"
+	source "$VIRTUAL_ENV"
+fi
